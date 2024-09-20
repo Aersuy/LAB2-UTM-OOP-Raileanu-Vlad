914827bd75bd41d83298a4f8a724399a7faba17d
Raileanu Vlad <raileanu1100@gmail.com>
2024-09-20 19:19:45 +0300

    finished constructor for stack

--- stack.cpp
+++ stack.cpp
@@ -3,4 +3,23 @@
 Stack::Stack()
 {
     c_maxSize = DEFAULT_STACK_SIZE;
+    c_top = -1;
+    c_elements = new int[DEFAULT_STACK_SIZE];
+}
+Stack::Stack(int size)
+{
+    c_maxSize = size;
+    c_top = -1;
+    c_elements = new int[size];
+}
+
+Stack::Stack(const Stack& copy)
+{
+     c_maxSize = copy.c_maxSize;
+     c_top = copy.c_top;
+     c_elements = new int[c_maxSize];
+     for (int i = 0; i <= c_top; i++)
+     {
+        c_elements[i] = copy.c_elements[i];
+     }
 }
